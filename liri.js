263a0d25ca60f6d23898d292f60f1b1c72d61d5d
Jaime Zepeda <nnk168@gmail.com>
2019-03-26 15:34:06 -0500

    added require lines

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+var keys = require("./keys.js")
+
+var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
+var moment = require("moment");
+var fs = require("fs");
+var axios = require("axios");
+
+var spotify = new Spotify(keys.spotify);
